b7159661ebf63b5f7865789d689e8555e64a331e
piush.bhandari <piush.bhandari@americaneagle.com>
2022-09-13 16:59:53 -0500

    finished commitment section

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Commitment from "../components/about/Commitment";
 import Hero from "../components/about/Hero";
 
 const About = () => {
   return (
     <main>
       <Hero />
+      <Commitment />
     </main>
   );
 };
